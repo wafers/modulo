9d4a8a197be5fbac75f89fafd24a70b87c0621c0
fmaredia <farhana.maredia@gmail.com>
2015-08-06 17:12:02 -0500

    Search results works with Angular

--- client/app/results/search.js
+++ client/app/results/search.js
@@ -2,12 +2,19 @@ angular.module('search', [])
 
 .controller('SearchController', function($scope, $http){
   $scope.searchInput = null;
+  $scope.results = [];
 
-  var getResults = function(){
-  	console.log("RUNNING")
+  $scope.getResults = function(){
+  	console.log("getting results")
+    console.log($scope.searchInput)
     return $http.post('/search', {'data': $scope.searchInput}).
       success(function(data, status, headers, config) {
         console.log(data);
+        for (var key in data) {
+          data[key].lastUpdate = moment(data[key].lastUpdate).format('l');
+          data[key].length = data[key].dependents.length;
+        }
+        $scope.results = data;
       }).
       error(function(data, status, headers, config) {
         console.log('error');
