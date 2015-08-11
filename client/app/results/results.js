cd9a12c15df2dae878b505c44033310e7effddd4
quikstrike <quikstrik3@gmail.com>
2015-08-11 18:50:14 -0500

    fixed grabing data from service on results controller

--- client/app/results/results.js
+++ client/app/results/results.js
@@ -1,10 +1,16 @@
 angular.module('app')
 
-.controller('ResultsController', ['Search', '$scope', '$http', function(Search, $scope, $http){
-  $scope.searchInput = Search.navInput;
-  $scope.results = function() { return Search.showResults() };
-  $scope.results();
-  $scope.toggle = function(result) {
-    result.show = !result.show;
-  }
+.controller('ResultsController', ['Search', '$scope', '$http', function(Search, $scope, $http) {
+    $scope.searchInput = Search.navInput;
+    
+    $scope.$watch(function() {
+            return Search.results.searchResults;
+        }, function() {
+            $scope.results = Search.showResults().searchResults
+        })
+        // function() { return Search.showResults() };
+        // $scope.results();
+    $scope.toggle = function(result) {
+        result.show = !result.show;
+    }
 }]);
