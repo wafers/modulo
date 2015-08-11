92ba343493821348fa0075b54400dbc7658dcb60
quikstrike <quikstrik3@gmail.com>
2015-08-10 19:53:41 -0500

    Fixed Querry Parsing, and Database Insertion

--- server/dbParsing.js
+++ server/dbParsing.js
@@ -1,30 +1,51 @@
 var helpers = require('./helpers.js')
 var config = require('./config').db
+var fs = require('fs')
 
-var dbLocal = require("seraph")({
+var dbRemote = require("seraph")({
     user: config.username,
-    pass: config.password
+    pass: config.password,
+    server: config.dbURL
 });
 
 var dependencys = {}
 
 var dbInsert = function(collection) {
     var finished = 0;
+
     for (var y = 0; y < collection.length; y++) {
         helpers.moduleDataBuilder(collection[y], function(err, data) {
             if (err) {
+                finished++;
+                if (finished === collection.length) {
+                    console.log("Done inserting nodes, now working on relationships.")
+                    relationshipInsert(dependencys)
+                }
                 return console.log("Oh, oh error")
             } else {
+                var querryString = "MATCH (n{name:{name}}) SET n.description = {description}, n.time = {time}, n.url = {url} , n.starred = {starred}, n.downloads = {downloads}, n.monthlyDownloadSum = {monthlyDownloadSum}"
+                console.log("Working on inserting into database. Finish count is", finished, "and y count is:", y)
                 dependencys[data.name] = data.dependents
-                dbLocal.save({
-                    "name": data.name,
-                    "description": data.description,
-                    "time": JSON.stringify(data.time),
-                    "url": data.url,
-                    "starred": data.starred.length,
-                    "downloads": JSON.stringify(data.downloads)
-                }, 'Module')
+                dbRemote.queryRaw(querryString, {
+                        name: data.name,
+                        description: data.description,
+                        time: JSON.stringify(data.time),
+                        url: data.url,
+                        starred: data.starred.length,
+                        downloads: JSON.stringify(data.downloads),
+                        monthlyDownloadSum: data.monthlyDownloadSum
+                    },
+                    function(err, node) {
+                        if (err) {
+                            console.log(err)
+                        } else {
+                            console.log("INSERTION SUCCESS")
+                        }
+                    })
+
+                finished++;
                 if (finished === collection.length) {
+                    console.log("Done inserting nodes, now working on relationships.")
                     relationshipInsert(dependencys)
                 }
             }
@@ -33,27 +54,53 @@ var dbInsert = function(collection) {
 }
 
 var relationshipInsert = function(collection) {
+    console.log("Inside relationshipsInsert")
+        // console.log(collection)
     for (var key in collection) {
-        for (var i; i < collection[key].length; i++) {
-            db.relate({
-                "name": collection[key][i]
-            }, 'DEPENDS_ON', {
-                "name": key
-            }, function(err, relationships) {
-                // relationships = all outgoing `knows` relationships from node 452
-            })
+        console.log("Inserting Relationship for ", key)
+        for (var i = 0; i < collection[key].length; i++) {
+            console.log("\t |")
+            console.log("\t |-> Relationship With", collection[key][i])
+
+            dbRemote.queryRaw("MATCH (n { name : '" + collection[key][i] + "'  }),(m { name: '" + key + "' }) CREATE (n)-[:DEPENDS_ON]->(m)",
+                function(err, result) {
+                    if (err) console.log(err);
+                    console.log(result)
+                })
         }
     }
 }
 
-var readInFile = function(path) {
-    fs.readFile(path, 'utf-8', function(err, results) {
-        var names = JSON.parse(results);
-        names = names.slice(0, 1000);
-        names.forEach(function(name) {
-            moduleDataBuilder(name, function(data) {
 
-            })
-        })
-    })
+var insertBatch = function(collection) {
+    var txn = dbRemote.batch();
+    var nodeArr = []
+    for (var a = 0; a < collection.length; a++) {
+        nodeArr.push(txn.save({
+            name: collection[a]
+        }));
+        if (nodeArr.length === 100) {
+            console.log((a / collection.length) * 100)
+            console.log("About to Insert Modules")
+            txn.label(nodeArr, 'Module');
+            txn.commit(function(err, results) {
+                if (err) console.log("Error", err)
+                console.log("Submited 100 Nodes");
+
+            });
+            nodeArr = []
+            txn = dbRemote.batch();
+        }
+    }
+    txn.label(nodeArr, 'Module');
+    txn.commit(function(err, results) {
+        if (err) console.log("Error", err)
+        console.log("Submited last " + nodeArr.length + "Nodes");
+    });
+
 }
+
+helpers.getAllNames(function(nameArr) {
+    // insertBatch(nameArr);
+    // dbInsert(nameArr.slice(150000, 160000))
+})
