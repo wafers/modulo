9f3eb625e1dfc3647b385434e77cc8a741348b49
WesTyler <westyler5@gmail.com>
2015-08-13 00:34:34 -0500

    Scale line graph to y-axis

--- client/app/results/services.js
+++ client/app/results/services.js
@@ -51,11 +51,13 @@ angular.module('app')
 .service('versionVis', function(){
   this.barGraphed = false;
   this.circleGraphed = false;
+  this.lineGraphed = false;
 
   this.resetGraphCheck = function() {
     console.log('Graph checks reset. Ready to draw.')
     this.barGraphed = false;
     this.circleGraphed = false;
+    this.lineGraphed = false;
   }
 
   this.circleGraph = function(module){
@@ -162,6 +164,123 @@ angular.module('app')
             .on('mouseout', tip.hide)
     }
     this.circleGraphed = true;
+  }
+
+  this.lineGraph = function(module){
+    if (!this.lineGraphed){
+      var dataStore = module.time;
+      var dateFormat = d3.time.format("%Y-%m-%dT%H:%M:%S.%LZ");
+      var updateCalculator = function(oldVersion, newVersion) {
+        newVersion = newVersion.split('.');
+        oldVersion = oldVersion.split('.');
+        var thisVersion = [(newVersion[0]-0)*50,(newVersion[1]-0)*5,(newVersion[2]*1-0)*0.5];
+        var lastVersion = [(oldVersion[0]-0)*50,(oldVersion[1]-0)*5,(oldVersion[2]*1-0)*0.5];
+        var thisTotal = thisVersion[0]+thisVersion[1]+thisVersion[2];
+        var lastTotal = lastVersion[0]+lastVersion[1]+lastVersion[2];
+        var versionDiff = thisTotal - lastTotal;
+        
+        return versionDiff > 0 ? versionDiff : 1;
+      }
+      var data = [];
+      var dataStoreArray = Object.keys(dataStore);
+      var last = '0.0.0'
+      for (var i=0; i<dataStoreArray.length; i++) {
+        if (dataStoreArray[i].split('.')[2]-0>=0){ // remove alpha/beta testing versions
+          var key = dataStoreArray[i];
+          var prevKey = last;
+          var versionObj = {};
+          if (key !== 'modified' && key !== 'created') {
+            versionObj['versionLabel'] = key;
+            versionObj['date'] = dateFormat.parse(dataStore[key]);
+            versionObj['versionValue'] = updateCalculator(prevKey, key);
+            versionObj['majorVersion'] = key.split('.')[0]
+            data.push(versionObj);
+            last = versionObj['versionLabel'];
+          }      
+        }
+      }
+      // data.shift()
+
+      var margin = {top: 50, right: 50, bottom: 50, left: 50},
+          width = 1000 - margin.left - margin.right,
+          height = 500 - margin.top - margin.bottom;
+          buckets = 4;
+          colors = ['000000', '315B7E', '1699C5', '00CCFF'];
+
+      var x = d3.time.scale() 
+        .range([0, width])
+        .domain([data[0]['date'], data.slice(-1)[0]['date']]);
+      var y = d3.scale.linear()
+        .range([height, 0])
+        .domain([0, d3.max(data, function(d) { return height/100 })]);
+      var colorScale = d3.scale.quantile()
+       .domain([0, buckets - 1, d3.max(data, function (d) { return d.majorVersion; })])
+       .range(colors);
+
+
+      var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient("bottom");
+      var yAxis = d3.svg.axis()
+          .scale(y)
+          .orient("left")
+          .ticks(5)
+
+      var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(function(d) {
+          var date = d.date.toString().substring(3,15);
+          var str = "<strong>Version:</strong> <span style='color:#00CCFF'>" + d.versionLabel + "</span>";
+          str += "</br><string>Date:</strong><span style='color:#00CCFF'> " + date + "</span>";
+          return str;
+        })
+      
+      // Create main svg for drawing 
+      var svg = d3.select(".versionVis").append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      svg.call(tip);
+      // Create y-axis and label
+      svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", -30)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Production Version");
+      // Create x-axis
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis)
+          // Rotate text labels
+          .selectAll('text')
+            .attr('x', 0)
+            .attr('dx', 15)
+            .attr('transform', 'rotate(30)')
+      
+      // Load in data and create circles for each
+      svg.selectAll(".circle")
+          .data(data)
+          .enter().append("circle")
+            .attr("class", "circle")
+            // Position circle at correct x position
+            .attr("cx", function(d) {return x(d.date)})
+            // Position circle at correct y-position
+            .attr("cy", function(d){return height - d.majorVersion*100})
+            // Size and color of circle based on update 'importance'
+            .attr("r", function(d){return (d.majorVersion+1) > 0 ? (d.majorVersion+1) : 1})
+            .attr("fill", function(d) {return '#'+colorScale(d.majorVersion)})
+            .attr("opacity", 0.7)
+            .on('mouseover', tip.show)
+            .on('mouseout', tip.hide)
+    }
+    this.lineGraphed = true;
   }
 
   this.barGraph = function(module){
