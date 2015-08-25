ffd837322212bae88d6242e2623cc7ef49ee1349
voxlol <voxlol@outlook.com>
2015-08-25 12:18:25 -0500

    Top 10 now only fetches if the data isnt there

--- client/app/topModules.js
+++ client/app/topModules.js
@@ -2,13 +2,19 @@ angular.module('app')
 
 .controller('TopModulesController', ['TopModules', '$scope', '$http', function(TopModules, $scope, $http){
   $scope.data;
-  TopModules.fetchTopModules();
+  $scope.properties;
+  if(_.isEmpty(TopModules.data)) TopModules.fetchTopModules();
 
   $scope.$watch(function(){ return TopModules.data }, function(){
     if(!_.isEmpty(TopModules.data)){
       // Watches the Graph service's selectedModule and sets it in the $scope if it exists
       $scope.data = TopModules.data;    
+      $scope.properties = Object.keys($scope.data).sort();   // alphabetical sorting
       console.log('retrieved top module data', $scope.data);
     }
   });
+
+  $scope.formatProperty = function(string){
+    
+  }
 }]);
