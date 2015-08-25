2300506bfba2c0345202e34b55a051c40676c74c
voxlol <voxlol@outlook.com>
2015-08-25 18:39:10 -0500

    Created second watcher function and commented topModules.js

--- client/app/topModules.js
+++ client/app/topModules.js
@@ -3,8 +3,11 @@ angular.module('app')
 .controller('TopModulesController', ['TopModules', 'Graph', '$scope', '$http', function(TopModules, Graph, $scope, $http){
   $scope.data;
   $scope.properties;
+
+  // Only fetch if the data is empty in the service
   if(_.isEmpty(TopModules.data)) TopModules.fetchTopModules();
 
+  // Watcher for return of Top 10 modules data
   $scope.$watch(function(){ return TopModules.data }, function(){
     if(!_.isEmpty(TopModules.data)){
       // Watches the Graph service's selectedModule and sets it in the $scope if it exists
@@ -17,16 +20,22 @@ angular.module('app')
         return element[0];
       });
 
-      TopModules.fetchTopModulesDownloadData(modulesObj);
-
-      // Send all the data packaged to Graph.topDownloadsGraph
-      // var width = document.getElementById('top-modules-graph').offsetWidth;
-      // Graph.topDownloadsGraph(modulesObj, width);
-
+      // Uses service function to call server endpoints
+      TopModules.fetchTopModulesDownloadData(modulesObj); 
     }
   });
 
-  
+  // Watcher for return of top 10 downloads data from server endpoint
+  $scope.$watch(function(){ return TopModules.topDownloadsData }, function(){
+    // compotuation when it changes
+
+    // Send all the data packaged to Graph.topDownloadsGraph
+    // var width = document.getElementById('top-modules-graph').offsetWidth;
+    // Graph.topDownloadsGraph(modulesObj, width);
+    
+  })
+
+
 
   $scope.formatProperty = function(string){
     var stringArr = string.replace(/([a-z](?=[A-Z]))/g, '$1 ').split('');
