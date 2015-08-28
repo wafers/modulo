ef30a0e7e1a93eceb32371fb6b340db99354f255
WesTyler <westyler5@gmail.com>
2015-08-28 11:26:38 -0500

    Move Top Modules Service to separate file

--- client/app/results/services.js
+++ client/app/results/services.js
@@ -648,34 +648,3 @@ angular.module('app')
     }
   }
 }])
-
-.service('TopModules',['$http', function($http) {
-  this.data = {};
-  this.topDownloadsData = {};
-
-  var topModulesService = this;
-
-  this.fetchTopModules = function(){
-    $http.get('/topModules').then(function(res){
-      topModulesService.data = res.data;
-    });
-  } 
-
-  this.fetchTopModulesDownloadData = function(moduleNames){
-    var data = {};
-    moduleNames.forEach(function(e){
-      $http.post('/detailedSearch', { data : e }).then(function(res){
-        // data[e] = _.pick(res.data, 'downloads');
-        // data[e] = JSON.parse(data[e].downloads);
-        data[e] = JSON.parse(res.data.downloads);
-
-        if(moduleNames.every(inDataObject)){
-          topModulesService.topDownloadsData = data;
-          console.log('data download', data);
-        }
-      });
-    });
-
-    function inDataObject(property){ return data.hasOwnProperty(property); }
-  }
-}]);
