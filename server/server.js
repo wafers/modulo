6365287044962d3462e67b671180e6b1a8bf43a2
WesTyler <westyler5@gmail.com>
2015-10-07 22:25:51 -0400

    Revert "Refactor logging to use Winston for integration with MongoDB"
    This reverts commit f7a93896721e3949f6d180a210fcc725af3763cd.

--- server/server.js
+++ server/server.js
@@ -4,10 +4,15 @@ var bodyParser = require('body-parser');
 var searchHelpers = require('./helpers.js');
 var routes = require('./routes.js');
 var cache = require('./cache.js');
-var winston = require('winston');
+var logger = require('morgan');
+var fs = require('fs');
 
 var app = express();
 
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
+logger.token('data', function(req, res){ return req.body ? 'Search Term: ' + JSON.stringify(req.body.data) : 'Other'; });
+
+app.use(logger(':method :url :data :response-time', {stream: accessLogStream}));
 app.use('/scripts', express.static(__dirname+'/../bower_components'));
 app.use(express.static(__dirname+'/../client'));
 app.use(bodyParser.json());
